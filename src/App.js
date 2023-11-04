b38e326e29e1bd9c377f17c4e443f61a3013dc5e
Galboa <121038807@upq.edu.mx>
2023-11-04 14:56:40 -0300

    Change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageDescription, setImageDescription] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Aquí agregarás la lógica para analizar la imagen con Azure AI Vision
+    // Puedes usar las funciones para hacer la solicitud al servicio de Azure AI Vision
+  };
+
+  const handleImageGeneration = () => {
+    // Aquí agregarás la lógica para generar una imagen con Azure OpenAI
+    // Puedes usar las funciones para hacer la solicitud al servicio de Azure OpenAI
+  };
+
+  return (
+    <div className="App">
+      <h1>Aplicación de Análisis y Generación de Imágenes</h1>
+      <input
+        type="text"
+        placeholder="URL de la imagen o descripción de texto"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
